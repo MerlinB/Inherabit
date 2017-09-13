b6b1abdce3fa34c36003282c28cf21cbb02c1457
MerlinB <merlin.buczek@gmail.com>
2017-09-13 17:38:03 +0200

    Modal

--- src/framework/forms.py
+++ src/framework/forms.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 #from dms.models import Controller
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    # first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    # last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
     # Additional Fields for Users?
@@ -17,7 +17,7 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
         
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=30)
