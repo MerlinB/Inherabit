8e4072251f7ddb9b629e846f39382ebf7b76ff75
MerlinB <merlin.buczek@gmail.com>
2017-09-13 16:41:17 +0200

    Header

--- src/dms/forms.py
+++ src/dms/forms.py
@@ -6,7 +6,9 @@ class DMSForm(forms.ModelForm):
     timeframe = forms.IntegerField(help_text='Days without contact until activation.')
     secret = forms.CharField(help_text='Your Secret.', widget=forms.Textarea)
     beneficiary = forms.EmailField(help_text='Who gets the secret?')
+    notification = forms.IntegerField(help_text='Days before activation when notification is send.')
     
+    # Old solution:
     # def __init__(self, user, *args, **kwargs):
     #     super(DMSForm, self).__init__(*args, **kwargs)
     #     self.controller = user.controller
